0231667901948f75b14d1982d6bea5f5678e1362
mihir.agrawal <mihir.agrawal@flipkart.com>
2026-05-24 17:31:27 +0530

    removed lombok completely due to some versioning conflicts

--- src/main/java/com/flipkart/learning/models/Device.java
+++ src/main/java/com/flipkart/learning/models/Device.java
@@ -3,11 +3,7 @@ package com.flipkart.learning.models;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.serde.annotation.Serdeable;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Serdeable
 @MappedEntity("device")
 public class Device{
@@ -21,4 +17,52 @@ public class Device{
     private long pingIntervalInSec;
 
     public Device(){}
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public long getPort() {
+        return port;
+    }
+
+    public void setPort(long port) {
+        this.port = port;
+    }
+
+    public long getPingIntervalInSec() {
+        return pingIntervalInSec;
+    }
+
+    public void setPingIntervalInSec(long pingIntervalInSec) {
+        this.pingIntervalInSec = pingIntervalInSec;
+    }
 }
--- src/main/java/com/flipkart/learning/models/MonitorLog.java
+++ src/main/java/com/flipkart/learning/models/MonitorLog.java
@@ -5,11 +5,7 @@ import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.data.annotation.Relation;
 import io.micronaut.serde.annotation.Serdeable;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Serdeable
 @MappedEntity("monitor_log")
 public class MonitorLog {
@@ -28,5 +24,59 @@ public class MonitorLog {
 
     public MonitorLog() {}
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getCpuUsage() {
+        return cpuUsage;
+    }
+
+    public void setCpuUsage(double cpuUsage) {
+        this.cpuUsage = cpuUsage;
+    }
+
+    public double getMemoryUsage() {
+        return memoryUsage;
+    }
+
+    public void setMemoryUsage(double memoryUsage) {
+        this.memoryUsage = memoryUsage;
+    }
+
+    public double getDiskUsage() {
+        return diskUsage;
+    }
+
+    public void setDiskUsage(double diskUsage) {
+        this.diskUsage = diskUsage;
+    }
+
+    public long getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(long uptime) {
+        this.uptime = uptime;
+    }
 }
