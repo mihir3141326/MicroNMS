29424880d7116aaf062df5d8428ea9c6afc8917b
mihir.agrawal <mihir.agrawal@flipkart.com>
2026-05-24 17:31:52 +0530

    added Service Layers

---
+++ src/main/java/com/flipkart/learning/services/DeviceService.java
@@ -0,0 +1,63 @@
+package com.flipkart.learning.services;
+
+import com.flipkart.learning.models.Device;
+import com.flipkart.learning.models.DeviceResponse;
+import com.flipkart.learning.repositories.DeviceRepository;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+import java.util.Optional;
+
+@Singleton
+public class DeviceService {
+
+    private final DeviceRepository deviceRepository;
+
+    // Micronaut automatically creates the repository object and injects into this class
+    public DeviceService(DeviceRepository deviceRepository) {
+        this.deviceRepository = deviceRepository;
+    }
+
+    public Device createDevice(Device device) {
+        // TODO: Check the ip address format, ping interval must be between 5 sec to 3600 seconds only (for simplicity), port can be positive number only
+        //TODO create new device entry in scheduler for monitoring
+        return deviceRepository.insert(device);
+    }
+
+    public Optional<Device> getDeviceByIp(String ip) {
+        return deviceRepository.findById(ip);
+    }
+
+    public boolean updateDevice(String ip, String username, String password, long port, long pingInterval) {
+        // TODO: ping interval must be between 5 sec to 3600 seconds only (for simplicity), port can be positive number only
+        if (deviceRepository.existsById(ip)) {
+            //TODO update device info in scheduler for monitoring
+            deviceRepository.updateByIp(ip, username, password, port, pingInterval);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteDevice(String ip) {
+        if (deviceRepository.existsById(ip)) {
+            deviceRepository.deleteById(ip);
+            return true;
+        }
+        return false;
+    }
+
+    public List<DeviceResponse> getAllDevices() {
+
+        // fetches all the data include sensitive info like username and password but don't send the info in the response
+        List<Device> allDevices = deviceRepository.findAll();
+
+        return allDevices.stream()
+                .map(device -> new DeviceResponse(
+                        device.getIp(),
+                        device.getTag(),
+                        device.getPort(),
+                        device.getPingIntervalInSec()
+                ))
+                .toList();
+    }
+}
---
+++ src/main/java/com/flipkart/learning/services/MonitorLogService.java
@@ -0,0 +1,56 @@
+package com.flipkart.learning.services;
+
+import com.flipkart.learning.models.Device;
+import com.flipkart.learning.models.MonitorLog;
+import com.flipkart.learning.repositories.DeviceRepository;
+import com.flipkart.learning.repositories.MonitorLogRepository;
+import io.micronaut.data.model.Page;
+import io.micronaut.data.model.Pageable;
+import jakarta.inject.Singleton;
+
+import java.util.List;
+import java.util.Optional;
+
+@Singleton
+public class MonitorLogService {
+
+    private final MonitorLogRepository logRepository;
+    private final DeviceRepository deviceRepository;
+
+    public MonitorLogService(MonitorLogRepository logRepository, DeviceRepository deviceRepository) {
+        this.logRepository = logRepository;
+        this.deviceRepository = deviceRepository;
+    }
+
+    public MonitorLog addLogToDevice(String deviceIp, MonitorLog log) {
+
+        Optional<Device> device = deviceRepository.findById(deviceIp);
+
+        if (device.isPresent()) {
+            log.setDevice(device.get());
+            if (log.getTimestamp() == 0) {
+                log.setTimestamp(System.currentTimeMillis());
+            }
+            return logRepository.save(log);
+        } else {
+            throw new IllegalArgumentException("Cannot save log: Device with IP " + deviceIp + " not found!");
+        }
+
+    }
+
+    public Page<MonitorLog> getLogsByDevice(String ip, int pageNumber, int pageSize) {
+        Pageable pageable = Pageable.from(pageNumber, pageSize);
+        return logRepository.findByDeviceIp(ip, pageable);
+    }
+
+    public List<MonitorLog> getLogsForLast24Hours(String ip) {
+        long twentyFourHoursAgo = System.currentTimeMillis() - (24 * 60 * 60 * 1000L);
+        return logRepository.findByDeviceIpAndTimestampGreaterThanEquals(ip, twentyFourHoursAgo);
+    }
+
+    public List<MonitorLog> getLogsForLastWeek(String ip) {
+        // 7 days * 24 hours * 60 mins * 60 secs * 1000 milliseconds
+        long oneWeekAgo = System.currentTimeMillis() - (7 * 24 * 60 * 60 * 1000L);
+        return logRepository.findByDeviceIpAndTimestampGreaterThanEquals(ip, oneWeekAgo);
+    }
+}
